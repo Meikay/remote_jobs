185e34b30e5b033c0162ff1ad5fd9e330f8ef023
Meikay <you@example.com>
2019-01-24 10:53:20 -0800

    updated environment(lib/remote_jobs.rb) and bin file executes CLI.rb

--- lib/remote_jobs.rb
+++ lib/remote_jobs.rb
@@ -1,5 +1,14 @@
-require "remote_jobs/version"
+#require 'nokogiri'
+#require 'open-url'
+#require 'pry'
 
-module RemoteJobs
+require_relative './remote_jobs/version.rb'
+require_relative './remote_jobs/cli'
+require_relative './remote_jobs/scraper'
+require_relative './remote_jobs/details'
+
+#module RemoteJobs
   # Your code goes here...
-end
+#end
+
+
