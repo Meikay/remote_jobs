14938c85d2acbb4c593c2ed4eaa6a93095a30fb8
Meikay <you@example.com>
2019-01-24 08:45:38 -0800

    setup my environment

---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'open-url'
+require 'pry'
+
+require_relative 'remote_jobs/version.rb'
+require_relative 'remote_jobs/cli'
+require_relative 'remote_jobs/scraper'
+require_relative 'remote_jobs/details'
