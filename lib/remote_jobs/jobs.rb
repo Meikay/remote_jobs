e28b260a8381a12b4db4bf30b855b9091a3f1cda
Meikay <you@example.com>
2019-01-25 19:11:47 -0800

    able to read out of objects when executing cli.rb

--- lib/remote_jobs/jobs.rb
+++ lib/remote_jobs/jobs.rb
@@ -1,9 +1,12 @@
+#what is this file doing?
+#calling RemoteJobs::Jobs.today doesnt work neither does running ./bin/console
+#check bin/remote_jobs for right to require
+
 class RemoteJobs::Jobs
     attr_accessor :name, :company, :language, :url
 
   def self.today
-    puts "1. JavaScript Developer - Muse - fullstack/React/Rails
-          2. Rails Developer - SUSE - ruby/JavaScript"
+    # Scrape remote_jobs and return list based on that data
 
           job_1 = self.new
           job_1.name = "JavaScript Developer"
@@ -11,5 +14,12 @@ class RemoteJobs::Jobs
           job_1.language = "fullstack/React/Rails"
           job_1.url = "https://remoteok.io/remote-jobs/70746-remote-full-stack-web-developer-suse"
 
+          job_2 = self.new
+          job_2.name = "Full Stack Developer"
+          job_2.company = "sulvo"
+          job_2.language = "fullstack/React/Rails"
+          job_2.url = "https://remoteok.io/remote-jobs/70634-remote-full-stack-developer-sulvo"
+
+          [job_1, job_2]
   end
 end
