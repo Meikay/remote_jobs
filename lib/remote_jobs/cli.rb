f85379aff27d1d3d4b29edcd97ea2ff06a18f1ff
Meikay <you@example.com>
2019-01-27 13:16:26 -0800

    recusion works and output is correct

--- lib/remote_jobs/cli.rb
+++ lib/remote_jobs/cli.rb
@@ -3,38 +3,54 @@ require 'pry'
 
 class RemoteJobs::CLI
     def start
-      jobs_list
+    #   jobs_list
       greet_user
       good_bye
     end
 
-    def jobs_list
-      #get jobs(scrape)
-      puts "Here are a list of jobs:"
-      @jobs = RemoteJobs::Scraper.scrape_jobs
-      @jobs.each.with_index(1) do |job, i|
-         puts "#{i}. #{job.name}" #- #{job.company} - #{job.language}
-      end
-    end
+    # def jobs_list
+    #   #get jobs(scrape)
+    #   puts "Here are a list of jobs:"
+    #   @jobs = RemoteJobs::Scraper.scrape_jobs
+    #   @jobs.each.with_index(1) do |job, i|
+    #      puts "#{i}. #{job.name}" #- #{job.company} - #{job.language}
+    #   end
+    # end
 
 
     def greet_user 
         input = nil
         while input != 'exit'
-        puts "Hi, Enter the number that you would like more information on or type list to see the list of jobs again! To exit, type exit:"
+        puts "Welcome to Remote Jobs"    
+        puts "Enter the number that you would like more information on" 
+        puts "Or type list to see the list of jobs again! To exit, type exit:"
         input = gets.strip.downcase
 
-        if input.to_i > 0
-            puts @jobs[input.to_i - 1]
-            puts "#{i}. #{job.name}"
-        elsif input == "list"
-            puts jobs_list 
-        else
-            puts "Not sure what you would like to do, please type: list, exit or a number!"
-            greet_user
+        # if input.to_i > 0
+        #     puts @jobs[input.to_i - 1]
+        #     puts "#{i}. #{job.name}"
+
+        case input
+        when "full stack"
+          puts "full stack"
+          scrape_full_stack
+          list_categories
+          choose_category
+        #   url = "https://remoteok.io/remote-full-stack-jobs"
+        #   RemoteJobs::Scraper.scrape_jobs(url) #list of full stack jobs scrape the full stack page
+    when "front end"
+        puts "front end"
+     when "rails"
+        puts "rails"
+     when "exit"
+        puts "Goodbye"
+     else
+       puts "Sorry! I didn't understand that input please enter a number or exit"
+       greet_user 
         end
-      end
     end
+end
+
 
     def good_bye
       puts "Thank you for checking our list of jobs!"
