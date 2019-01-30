1ed864e772ff75566e96fede26f392cce3058f9e
Meikay <you@example.com>
2019-01-30 08:06:44 -0800

    print_jobs method works at the right moment

--- lib/remote_jobs/cli.rb
+++ lib/remote_jobs/cli.rb
@@ -1,12 +1,57 @@
 require 'pry'
 #CLI Controller
 
-class RemoteJobs::CLI
-    def start
-    #   jobs_list
-      greet_user
-      good_bye
+ class RemoteJobs::CLI
+
+     def start
+       greet_user
+       RemoteJobs::Scraper.full_stack_jobs
+       print_jobs
+       good_bye
     end
+
+    def greet_user 
+        input = nil
+        while input != 'exit'
+        puts "Welcome to Full Stack Remote Jobs"    
+        puts "Enter the number of the job you would like more information on" 
+        puts "Or type list to see the list of Full Stack Remote Jobs again! To exit, type exit:"
+        input = gets.strip.downcase
+
+        if input == "full stack"
+          RemoteJobs::Scraper.full_stack_jobs  #puts a numbered list of full stack jobs 
+          print_jobs
+          binding.pry
+        #elsif input.to_i > 0 
+            #puts out the details about the job the user chose
+        elsif input == "exit"
+          good_bye
+        else
+          puts "Sorry! I didn't understand that input please enter a number or exit"
+          greet_user 
+        end
+      end
+    end
+
+    def print_jobs
+      puts ""
+      puts "---------- Full Stack Jobs ----------"
+      puts ""
+      RemoteJobs::Details.all.each.with_index(1) do |job, index| #use a range
+        puts "#{index}. #{job.name} - #{job.company} - #{job.language} - #{job.url}"
+       
+      end
+    end
+ 
+
+     def good_bye
+       puts "Thank you for checking our list of jobs!"
+     end
+ end
+
+
+ 
+
 
     # def jobs_list
     #   #get jobs(scrape)
@@ -16,46 +61,4 @@ class RemoteJobs::CLI
     #      puts "#{i}. #{job.name}" #- #{job.company} - #{job.language}
     #   end
     # end
-
-
-    def greet_user 
-        input = nil
-        while input != 'exit'
-        puts "Welcome to Remote Jobs"
-        puts "Type in the type of job you are looking for"    
-        puts "Enter the number that you would like more information on" 
-        puts "Or type list to see the list of jobs again! To exit, type exit:"
-        input = gets.strip.downcase
-
-        # if input.to_i > 0
-        #     puts @jobs[input.to_i - 1]
-        #     puts "#{i}. #{job.name}"
-
-        case input
-        when "full stack"
-          puts "full stack"
-        #   scrape_full_stack
-        #   list_numbered_jobs
-        #   choose_number
-        #    url = "https://remoteok.io/remote-full-stack-jobs"
-        #    jobs = RemoteJobs::Scraper.scrape_jobs(url) #list of full stack jobs scrape the full stack page
-           
-        when "front end"
-          puts "front end"
-        when "rails"
-          puts "rails"
-        when "exit"
-          puts "Goodbye"
-        else
-          puts "Sorry! I didn't understand that input please enter a number or exit"
-          greet_user 
-        end
-    end
-end
-
-
-    def good_bye
-      puts "Thank you for checking our list of jobs!"
-    end
-end
 
--- lib/remote_jobs/scraper.rb
+++ lib/remote_jobs/scraper.rb
@@ -2,49 +2,53 @@ require 'pry'
 class RemoteJobs::Scraper
     attr_accessor :name, :company, :language, :url
 
-     doc = Nokogiri::HTML(open("https://remoteok.io/remote-full-stack-jobs"))
-     job_listings = doc.css("td.company.position.company_and_position")
-        job_listings.map do |job_listing|
-          name = job_listing.css('a.preventLink h2').text, #lists all objects not just name,use .shift maybe iterate and extract the  object
-          company = job_listing.css('a.companyLink h3').text,
-          language = job_listing.css('div.tag.tag-full-stack h3').text,  #broken
-          url = "https://remoteok.io" + job_listing.css('a')[0].attributes["href"].value
-          
-          binding.pry
-        end
+    def self.full_stack_jobs
+      doc = Nokogiri::HTML(open("https://remoteok.io/remote-full-stack-jobs"))
+        job_listings = doc.css("tr.job") #td.company.position.company_and_position
+          job_listings.each do |job_listing|
+           name = job_listing.css('td.company.position.company_and_position a.preventLink h2').text
+           company = job_listing.css('a.companyLink h3').text,
+           language = job_listing.css('td.tags').text,   
+           url = "https://remoteok.io" + job_listing.css('a')[0].attributes["href"].value
+            
+          end
 
+    def self.scrape_link(url)
 
-      # How I want my data to look like
-      # job_listings = parsed_page.css('td.company.position.company_and_position')
-      # job_listings.each do |job_listing|
-      #   job = {
-      #     name: job_listing.css('a.preventLink h2').text,
-      #     company: job_listing.css('a.companyLink h3').text,
-      #     language: job_listing.css('div.tag tag-full-stack h3').text,
-      #     url: "https://remoteok.io" + job_listing.css('a')[0].attributes["href"].value
-    
+    end
 
-
-  # def self.scrape_details(url)
-  #   # Scrape remote_jobs and return list based on that data
-  #   webpage = Nokogiri::HTML(open(url))
-  #   array_of_jobs = webpage.css("td.company.position.company_and_position")
-  #      title = array_of_jobs.css("a.preventLink h2")
-    
-  #     array_of_jobs.map do |job|
-  #       #RemoteJobs::Details.new(job.text) # lists the non-full stack dev jobs
-  #          title = job.css("a.preventLink h2").text # lists full stack dev jobs but I need the title only
-  #           url = job.css("a.preventLink")[0]['href']
-  #           puts url
-  #     end
-  # end
-
-  # def scrape_jobs(details)
-  #    webpage = Nokogiri::HTML(open(details.url))
-  #    jobs = webpage.css("a.preventLink h2")
-  #    jobs.each do |job_link|
-  #    details.jobs << job_link.text 
-  #   end
-  # end
 end
 
+
+
+
+
+
+
+
+
+
+
+end
+  # def self.full_stack_jobs  
+  #   listing = []
+  #    doc = Nokogiri::HTML(open("https://remoteok.io/remote-full-stack-jobs"))
+  #    job_listings = doc.css("tr.job") #td.company.position.company_and_position
+  #       job_listings.each do |job_listing|
+  #         jobs = {}
+  #         jobs[:name] = job_listing.css('a.preventLink h2').text, #lists all objects not just name,use .shift maybe iterate and extract the  object
+  #         jobs[:company] = job_listing.css('a.companyLink h3').text,
+  #         jobs[:language] = job_listing.css('td.tags').text,   
+  #         jobs[:url] = "https://remoteok.io" + job_listing.css('a')[0].attributes["href"].value
+  #         listing << jobs
+  #       end
+  #     listing  
+  # end
+
+  # def self.scrape_details(url)
+  #   webpage = Nokogiri::HTML(open(url))
+  #   deets = webpage.css("a.preventLink href")
+  #     deets.each do |job_deets|
+  #       url.deets << job_deets.text
+  #     end
+  # end
