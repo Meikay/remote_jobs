14c3ef492cd726ce3b03b986147314dc5355f162
Meikay <you@example.com>
2019-03-13 20:18:52 -0700

    Fix exit and indentation issues

--- lib/remote_jobs/cli.rb
+++ lib/remote_jobs/cli.rb
@@ -15,23 +15,24 @@ class RemoteJobs::CLI
   def greet_user
     puts "Welcome To Our Top 100 Full Stack Remote Jobs From Remoteok.io".colorize(:yellow)
     puts "Would you like to see our list?".colorize(:yellow)
-    puts "If so, please type 'Yes'!".colorize(:yellow)
+    puts "If so, please type 'Yes' or 'Y'!".colorize(:yellow)
   end
 
   def menu
       input = nil
       while input != 'exit'    
       input = gets.strip.downcase
-      case input
-      when "yes"  
+      if input == "yes" || input == "y"
         menu_helper
-      when "exit"
-        good_bye
+      elsif input == "exit" || input == "e"
+        good_bye         
       else
         puts "Sorry! I didn't understand that input please enter a number or exit".colorize(:yellow)
-        menu
+        puts "Would you like to see our list?".colorize(:yellow)
+        puts "If so, please type 'Yes' or 'Y'!".colorize(:yellow)
+        menu 
       end
-    end
+      end
   end
 
   def display_jobs
@@ -39,44 +40,48 @@ class RemoteJobs::CLI
     puts "---------- Top 100 Full Stack Jobs ----------".colorize(:yellow)
     puts ""
     RemoteJobs::Jobs.all.each.with_index(1) do |job, index| #use a range
-        puts "#{index}. #{job.name} - #{job.company} - #{job.url}".colorize(:red)
+      puts "#{index}. #{job.name} - #{job.company} - #{job.url}".colorize(:red)
     end
   end
 
   def choose_num
     puts "\nIf you would like to know more information about a job, choose a number from 1-100:".colorize(:yellow)
-      input = gets.strip
-      until input.to_i.between?(1,100) || input == "exit"
-        puts "Sorry! I didn't understand that, please enter a number or exit!".colorize(:yellow)
-        input = gets.strip
+    input = gets.strip
+    until input.to_i.between?(1,RemoteJobs::Jobs.all.length)  #it scrapes when it should exit!
+      if input == "e" || input == "exit" 
+        good_bye
+      else  
+        puts "Sorry! I didn't understand that...".colorize(:yellow)
+        choose_num
       end
-      index = input.to_i - 1
-      jobs = RemoteJobs::Jobs.all
-      if jobs[index].description == nil  #if job object does not have a description
-        RemoteJobs::Scraper.add_description(RemoteJobs::Jobs.all[index]) #only do this if the job I picked does not have a description
-      end
-      clean_description = jobs[index].description.gsub("{linebreak}", "\n")
-      puts "#{clean_description}".colorize(:blue) 
-      alt_menu
+    end
+    index = input.to_i - 1
+    jobs = RemoteJobs::Jobs.all
+    if jobs[index].description == nil  #if job object does not have a description
+      RemoteJobs::Scraper.add_description(RemoteJobs::Jobs.all[index]) #only do this if the job I picked does not have a description
+    end
+    clean_description = jobs[index].description.gsub("{linebreak}", "\n")
+    puts "#{clean_description}".colorize(:blue) 
+    alt_menu
   end 
   
 
   def alt_menu
     puts "Would you like to see another job description? Type 'D' ".colorize(:yellow)
-    puts "Would you like to exit? Type 'E' ".colorize(:yellow)
+    puts "Would you like to exit? Type 'E' or 'Exit' ".colorize(:yellow)
     input = nil
     while input != 'exit'    
     input = gets.strip.downcase
     case input
-    when "d"
+    when "d" || "description"
       menu_helper
-    when "e"
+    when "e" || "exit"
       good_bye
     else
-      puts "Sorry! I didn't understand that, please enter a number or exit.".colorize(:yellow)
+      puts "Sorry, I didn't understand that...".colorize(:yellow)
       alt_menu  
     end
-  end
+    end
   end
 
   def menu_helper
